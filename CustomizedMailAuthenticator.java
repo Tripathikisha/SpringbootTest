82424b3ec99a2f06cf5e06fbf965b27ca04c9efc
Kishan Tripathi <102147410+Tripathikisha@users.noreply.github.com>
2023-09-10 16:14:12 +0530

    Add files via upload

---
+++ CustomizedMailAuthenticator.java
@@ -0,0 +1,6 @@
+package org.example;
+
+import javax.mail.Authenticator;
+
+public class CustomizedMailAuthenticator extends Authenticator {
+}
---
+++ MailConstants.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class MailConstants {
+
+    public static final String SENDER = "tripathi.kishan10@gmail.com";
+    public static final String APP_PASSWORD = "123ghj";
+}
